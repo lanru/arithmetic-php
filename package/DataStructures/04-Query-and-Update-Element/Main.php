1c655861cc32a62cad14029146c4fee8edc50f1a
17381792 <17381792@qq.com>
2018-10-25 08:18:59 +0800

    init

--- package/DataStructures/04-Query-and-Update-Element/Main.php
+++ package/DataStructures/04-Query-and-Update-Element/Main.php
@@ -7,22 +7,26 @@
  */
 require_once "ArrayQueue.php";
 require_once "LoopQueue.php";
-
+// 毫秒级时间戳
+function getMillisecond() {
+    list($t1, $t2) = explode(' ', microtime());
+    return (float)sprintf('%.0f',(floatval($t1)+floatval($t2))*1000);
+}
 function testQueue($count)
 {
-    $startTime = time();
-    $q = new ArrayQueue(10);
+    $startTime = getMillisecond();
+    $q = new LoopQueue(10);
     for ($i = 0; $i < $count; $i++) {
         $q->enqueue($i);
     }
     for ($i = 0; $i < $count; $i++) {
         $q->dequeue();
     }
-    $endTime = time();
-    $time = ($endTime - $startTime);
-    echo "used time:$time s";
+    $endTime = getMillisecond();
+    $time = $endTime - $startTime;
+    echo "used time:$time ms";
 }
 
-$count = 10000;
+$count = 100000;
 testQueue($count);
 
