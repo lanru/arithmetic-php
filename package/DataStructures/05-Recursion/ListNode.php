32e6f1974db56171159fc0b6f0ebe2f86119619c
17381792 <17381792@qq.com>
2018-10-27 00:21:52 +0800

    init

--- package/DataStructures/05-Recursion/ListNode.php
+++ package/DataStructures/05-Recursion/ListNode.php
@@ -15,4 +15,53 @@ class ListNode
     {
         $this->val = $x;
     }
+
+    public function init(array $arr)
+    {
+        if ($arr == null || count($arr) == 0) {
+            throw  new Exception("");
+        }
+        $this->val = $arr[0];
+        $cur = $this;
+        for ($i = 1; $i < count($arr); $i++) {
+            $cur->next = new ListNode($arr[$i]);
+            $cur = $cur->next;
+        }
+    }
+
+    public function __toString()
+    {
+        $res = "";
+        $cur = $this;
+        while ($cur != null) {
+            $res .= $cur->val . "->";
+            $cur = $cur->next;
+        }
+        $res .= "NULL\n";
+        return $res;
+    }
 }
+
+function removeEle(ListNode $head, $val)
+{
+    while ($head != null && $head->val == $val) {
+        $head = $head->next;
+    }
+    if ($head == null) {
+        return null;
+    }
+    $prev = $head;
+    while ($prev->next != null) {
+        if ($prev->next->val == $val) {
+            $prev->next = $prev->next->next;
+        } else {
+            $prev = $prev->next;
+        }
+    }
+    return $head;
+}
+
+$nums = [1, 2, 6, 3, 4, 5, 6];
+$head = new ListNode(1);
+$head->init($nums);
+echo $head;
