26136ad2fcda41f303ee6f3b6bc7ab0081b54939
17381792 <17381792@qq.com>
2018-10-26 23:03:38 +0800

    init

---
+++ package/DataStructures/05-Recursion/ListNode.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wlh
+ * Date: 2018/10/26
+ * Time: 11:02 PM
+ */
+
+class ListNode
+{
+    public $val;
+    public $next;
+
+    public function __construct($x)
+    {
+        $this->val = $x;
+    }
+}
