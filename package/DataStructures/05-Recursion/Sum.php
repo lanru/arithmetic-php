7cb8f888cc75584a4a22811d11c4fb2174f30d7d
17381792 <17381792@qq.com>
2018-10-28 21:28:34 +0800

    init

--- package/DataStructures/05-Recursion/Sum.php
+++ package/DataStructures/05-Recursion/Sum.php
@@ -10,14 +10,17 @@ function sum(array $arr)
     return subsum($arr, 0);
 }
 
-function subsum(array $arr, $l)
+function subsum(?array $arr, ?int $l)
 {
     if ($l == count($arr)) { //求解最基本问题
         return 0;
     }
-    return $arr[$l] + subsum($arr, $l + 1);
+    $x = subsum($arr, $l + 1);
+    $res = $arr[$l] + $x;
+    return $res;
     // 把原问题转化成更小的问题
 }
-$nums = [1, 2, 3, 4, 5, 6, 7, 8];
+
+$nums = [6,10];
 $num = sum($nums);
 echo $num;
